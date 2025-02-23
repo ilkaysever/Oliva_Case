35a65bb3fa3a49125a4a6d1e93919a141b830eee
İlkay Sever <ilkay.sever@vakifbank.com.tr>
2025-02-23 13:21:52 +0300

    Oliva_Case Project Created

---
+++ Oliva_Case/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Oliva_Case
+//
+//  Created by İlkay SEVER (Mobil Uygulamalar Uygulama Geliştirme Müdürlüğü) on 23.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Oliva_CaseTests/Oliva_CaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Oliva_CaseTests.swift
+//  Oliva_CaseTests
+//
+//  Created by İlkay SEVER (Mobil Uygulamalar Uygulama Geliştirme Müdürlüğü) on 23.02.2025.
+//
+
+import Testing
+@testable import Oliva_Case
+
+struct Oliva_CaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
