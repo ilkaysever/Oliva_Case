bb8cd34f1de18978edc4b12b367d5696966dd3c1
İlkay Sever <ilkay.sever@vakifbank.com.tr>
2025-02-23 14:44:43 +0300

    coreData & favorite screen

--- Oliva_Case/Scene/FavoriteScreen/FavoriteController.swift
+++ Oliva_Case/Scene/FavoriteScreen/FavoriteController.swift
@@ -1,5 +1,5 @@
 //
-//  HomeController.swift
+//  FavoriteController.swift
 //  Oliva_Case
 //
 //  Created by İlkay SEVER (Mobil Uygulamalar Uygulama Geliştirme Müdürlüğü) on 23.02.2025.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-final class HomeController: BaseViewController {
+final class FavoriteController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .yellow
     }
     
 }
